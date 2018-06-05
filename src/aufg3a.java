b4ea5a0b9bfe32ce11a3b07d5b9017eab6165fa3
Saskia <sassi172@web.de>
2018-06-05 19:21:54 +0200

    Aufgabe 3a denke mal fertig

--- src/aufg3a.java
+++ src/aufg3a.java
@@ -1,80 +1,71 @@
 
 public class aufg3a {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String []args){
 
-		
-		
-	}
-	
-	// umwandeln in binaer
-	public static String toBinary(int zahl){
-		if(zahl < 2){
-			return "" + zahl;
-		}else{
-			return toBinary(zahl/2) + zahl%2;
-		}
-	}
-	
-	// umdrehen der strings um spaeter Nullen nicht zu verlieren
-	public static String reverseString(String bin) {
-		String reversed = "";
-		
-		 for( int j = bin.length()-1; j >= 0; j-- ) {
-		      reversed += bin.charAt(j);
-		 } 
-		 
-		 return reversed;
-	}
+		int a=7;
+		int b=5;
+		int c=3;
+		int d=1;
+		//  int [][][][] tabelle= new int [4][4][4][4];
+		int []befuellungA=new int [a];
+		int []befuellungB=new int [b];
+		int []befuellungC=new int [c];
+		int []befuellungD=new int [d];
+		//    int x=3;
+		boolean [][][][] tabelle2= new boolean [a][b][c][d];
+		toBinary(a,befuellungA,a-1);
+		toBinary(b,befuellungB,b-1);
+		toBinary(c,befuellungC,c-1);
+		toBinary(d,befuellungD,d-1);
 
-
-
-
-	public boolean gewinnPosition( int a, int b, int c, int d){
-		
-		boolean gewinn = false;
-		int binLength = 0;
-		int counter=0;
-		int geradeAnz=0;
-		
-		
-		String[] table = { toBinary(a), toBinary(b), toBinary(c), toBinary(d)};
-		
-		// finde groesste Anzahl der Ziffern der Binaerzahlen
-		for(int i=0; i< table.length; i++) { 
-			if(table[i].length()>= binLength) {
-				binLength = table[i].length();
-			}	
-		}
-		
-		// fuellt die Binaerzahl mit Nullen auf falls notwendig
-		for(int i= 0; i< table.length; i++) {
-			while(table[i].length() < binLength) {
-				table[i] = "0" + table[i];
+		for(int i=0;i<a;i++){
+			for(int j=0;j<b;j++){
+				for(int k=0;k<c;k++){
+					for(int l=0;l<d;l++){
+						if((befuellungA[i]+befuellungB[j]+befuellungC[k]+befuellungD[l])%2==0){
+							tabelle2[i][j][k][l]=true;
+						}
+						else{
+							tabelle2[i][j][k][l]=false;
+						}
+						System.out.println("Position an "+(i+1)+" "+(j+1)+" "+(k+1)+" "+(l+1)+":  "+tabelle2[i][j][k][l]);
+					}
+				}
 			}
 		}
-		
-		int [][] binTable = new int[binLength][binLength];
-		
-		
-		for(int i=0; i< table.length; i++) {
-			for(int j=0; j<binTable.length; j++ ) {
-				
-			}
-			
-		}
-	
 
-		
-		
-		return gewinn;
-		
-		
-	
-		
-		
-		
+
+
+
+     /*   for(int a=0;a<4;a++){
+            toBinary(7,befuellungA,x);
+            tabelle[a][0][0][0]=befuellungA[a];
+            for(int b=0;b<4;b++){
+                toBinary(5,befuellungB,x);
+                tabelle[a][0][0][b]=befuellungB[b];
+                for(int c=0;c<4;c++){
+                    for(int d=0;d<4;d++){
+
+                    }
+                }
+
+            }
+        } */
+
 	}
-	
+
+	public static void toBinary(int zahl,int [] befuellung,int x){
+
+		if(zahl<2){
+			befuellung[x]=zahl;
+		}
+		else{befuellung[x]=+zahl%2;
+			toBinary(zahl/2,befuellung,x-1);
+		}
+
+
+
+
+	}
 }
