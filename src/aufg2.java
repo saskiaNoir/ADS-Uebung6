8a561fdafbf82d0c1662d30c353a525d1d44ec69
Saskia <sassi172@web.de>
2018-06-05 19:46:57 +0200

    aufgabe 2 hinzugefügt

---
+++ src/aufg2.java
@@ -0,0 +1,47 @@
+import de.medieninf.ads.ADSTool;
+
+public class aufg2 {
+    public static void main(String []args){
+
+        int z1=0;
+        int z2=0;
+        int sub=0;
+        int x= 5260;
+        int erga=-1;
+        int ergebB=-1;
+
+
+        int array []= ADSTool.readIntArray("L.dat");
+        //   int array2 []={2,3,5,7,11,15,20};
+
+        for(int i=array.length-1;i>z1;i--){
+            z1=0;
+            z2=array[i];
+            if(z2<=x){
+                erga= rechne2(z1,z2,sub,erga,x,array);
+                if(erga>=0){
+                    ergebB=z2;
+                    System.out.println("a: "+erga+"   b: "+ergebB+"  ergibt: "+x);
+                }
+            }
+
+        }
+
+
+    }
+
+
+
+    public static int rechne2(int z1,int z2,int sub,int erga,int x,int[]array) {
+
+        sub = x - array[z1];
+        if (z2 == sub) {
+
+            return array[z1];
+        } else if (sub > z2) {
+            return rechne2(z1 + 1, z2, sub, erga, x, array);
+        } else {
+            return rechne2(z1, z2 - 1, sub, erga, x, array);
+        }
+    }
+    }
