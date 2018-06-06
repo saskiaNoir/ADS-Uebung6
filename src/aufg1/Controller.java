b79ddc8b73adca230da67452a6b076345d3b902f
Paul Schade <paulschade98@googlemail.com>
2018-06-06 19:41:49 +0200

    added args param

--- src/aufg1/Controller.java
+++ src/aufg1/Controller.java
@@ -1,24 +1,31 @@
 package aufg1;
+
 import de.medieninf.ads.ADSTool;
 
 public class Controller {
 	public static void main(String[] args) {
 
-		int i = 0;
-		String array[] = ADSTool.readWordArray("words.dat");
-		PriorityQueue pQueue = new PriorityQueue();
-		for (String element : array) {
-			i++;
-			pQueue.insert(new Element(element));
-			// System.out.println(element);
+		try {
+			String args1 = args[0];
+
+			int i = 0;
+			String array[] = ADSTool.readWordArray(args1);
+			PriorityQueue pQueue = new PriorityQueue();
+
+			for (String element : array) {
+				i++;
+				pQueue.insert(new Element(element));
+				// System.out.println(element);
+			}
+
+			for (int j = 0; j < pQueue.length(); i++) {
+
+				System.out.println(pQueue.min());
+				pQueue.getMin();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		for (int j = 0; j < pQueue.length(); i++) {
-
-			System.out.println(pQueue.min());
-			pQueue.getMin();
-
-		}
-
 	}
 }
--- src/aufg2/aufg2.java
+++ src/aufg2/aufg2.java
@@ -1,48 +1,50 @@
 package aufg2;
+
 import de.medieninf.ads.ADSTool;
 
 public class aufg2 {
-    public static void main(String []args){
+	public static void main(String[] args) {
 
-        int z1=0;
-        int z2=0;
-        int sub=0;
-        int x= 5260;
-        int erga=-1;
-        int ergebB=-1;
+		try {
+			String args1 = args[0];
 
+			int z1 = 0;
+			int z2 = 0;
+			int sub = 0;
+			int x = 5260;
+			int erga = -1;
+			int ergebB = -1;
 
-        int array []= ADSTool.readIntArray("L.dat");
-        //   int array2 []={2,3,5,7,11,15,20};
+			int array[] = ADSTool.readIntArray(args1);
+			// int array2 []={2,3,5,7,11,15,20};
 
-        for(int i=array.length-1;i>z1;i--){
-            z1=0;
-            z2=array[i];
-            if(z2<=x){
-                erga= rechne2(z1,z2,sub,erga,x,array);
-                if(erga>=0){
-                    ergebB=z2;
-                    System.out.println("a: "+erga+"   b: "+ergebB+"  ergibt: "+x);
-                }
-            }
+			for (int i = array.length - 1; i > z1; i--) {
+				z1 = 0;
+				z2 = array[i];
+				if (z2 <= x) {
+					erga = rechne2(z1, z2, sub, erga, x, array);
+					if (erga >= 0) {
+						ergebB = z2;
+						System.out.println("a: " + erga + "   b: " + ergebB + "  ergibt: " + x);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        }
+	}
 
+	public static int rechne2(int z1, int z2, int sub, int erga, int x, int[] array) {
 
-    }
+		sub = x - array[z1];
+		if (z2 == sub) {
 
-
-
-    public static int rechne2(int z1,int z2,int sub,int erga,int x,int[]array) {
-
-        sub = x - array[z1];
-        if (z2 == sub) {
-
-            return array[z1];
-        } else if (sub > z2) {
-            return rechne2(z1 + 1, z2, sub, erga, x, array);
-        } else {
-            return rechne2(z1, z2 - 1, sub, erga, x, array);
-        }
-    }
-    }
+			return array[z1];
+		} else if (sub > z2) {
+			return rechne2(z1 + 1, z2, sub, erga, x, array);
+		} else {
+			return rechne2(z1, z2 - 1, sub, erga, x, array);
+		}
+	}
+}
